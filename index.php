ca98a4c906ba7571059fecf493442949974e455e
Nokx1z <enyelldev@gmail.com>
2026-01-19 19:02:21 -0400

    Cambios en la ruta del front controller

--- index.php
+++ index.php
@@ -1,9 +1,8 @@
 <?php
 namespace Barkios;
-//require_once __DIR__ . '/controllers/FrontController.php';
-require_once 'vendor/autoload.php';
 
-// Instancia y ejecución del FrontController
+// Usar __DIR__ asegura que PHP busque desde la carpeta donde está este index.php
+require_once __DIR__ . '/vendor/autoload.php';
 
 use Barkios\controllers\FrontController;
 
